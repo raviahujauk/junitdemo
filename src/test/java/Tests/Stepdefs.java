91cf187d615f0873d81631adb678b0c4213be18e
raviahujauk <34134515+raviahujauk@users.noreply.github.com>
2020-05-07 22:18:16 +0100

    uncomment headlesss steps

--- src/test/java/Tests/Stepdefs.java
+++ src/test/java/Tests/Stepdefs.java
@@ -14,12 +14,14 @@ public class Stepdefs {
     public void iGoTo(String arg0) throws Throwable {
         //For Windown
         //System.setProperty("webdriver.chrome.driver" , "chromedriver.exe");
+
         //For Linux
         System.setProperty("webdriver.chrome.driver" , "/usr/local/bin/chromedriver");
         //Headless browser
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
-        //driver = new ChromeDriver(options);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
         driver = new ChromeDriver();
         driver.get("https://www.youtube.com/channel/UCSMFeUcRuAqIGQu3RWOlCRg");
     }
